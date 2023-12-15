4ad80ccd2ac9e172311df3cc9ad01a94ba77bd9f
mxnan <128995981+mxnan@users.noreply.github.com>
2023-12-15 21:17:37 +0530

    site-done,responsiveness and final touch

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import DisplaySection from "./components/DisplaySection";
+import Jumbotron from "./components/Jumbotron";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+import WebgiViewer from "./components/WebgiViewer";
+
+function App() {
+  const WebgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    WebgiViewerRef.current.triggerPreview();
+  };
+
+  return (
+    <div className="App">
+      <div id="content" ref={contentRef}>
+        <Nav />
+        <Jumbotron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+
+      <WebgiViewer contentRef={contentRef} ref={WebgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
